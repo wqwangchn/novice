d7ab9a4c27eef2a78b7421426aba8adae6804357
wqwang <wenqiangw@opera.com>
2020-03-21 10:30:55 +0800

    init novice

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name='novice',
+	version='0.0.1',
+	description='data mining for novice and expert',
+	url='https://github.com/wqwangchn/novice',
+	author='wqwangchn',
+	author_email='wqwangchn@163.com',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	"Programming Language :: Python :: 3",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+	],
+	zip_safe=False
+)
