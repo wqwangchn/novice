bd4b71da00c7f63a5d87737ba7f3577331111584
wqwang <wenqiangw@opera.com>
2020-03-27 14:05:10 +0800

    update

--- build/lib/pynovice/bert_extracter/__init__.py
+++ build/lib/pynovice/bert_extracter/__init__.py
@@ -7,3 +7,4 @@ Email: wenqiangw@opera.com
 Date: 2020-03-25 14:40
 Desc:
 '''
+from bert_extracter import ExtractBertFeatures,DownloadBertModel
--- pynovice/bert_extracter/bert_extracter.py
+++ pynovice/bert_extracter/bert_extracter.py
@@ -7,24 +7,25 @@ Email: wenqiangw@opera.com
 Date: 2020-03-25 14:40
 Desc:
 '''
-import zipfile
-
 from bert4keras.models import build_transformer_model
 from bert4keras.tokenizers import Tokenizer
 import numpy as np
 import os
 import requests
+import zipfile
+
+CUR_PASTH = os.path.dirname(os.path.abspath(__file__))
 
 class ExtractBertFeatures:
-    def __init__(self,bert_model_dir = './uncased_L-2_H-128_A-2'):
-        self.model_dir = bert_model_dir
-        self.load_model()
+    def __init__(self):
+        bert_model_dir = os.path.join(CUR_PASTH,'data','uncased_L-2_H-128_A-2')
+        self.load_model(model_dir=bert_model_dir)
         pass
 
-    def load_model(self):
-        config_name = os.path.join(self.model_dir, 'bert_config.json')
-        checkpoint_name = os.path.join(self.model_dir, 'bert_model.ckpt')
-        vocab_name = os.path.join(self.model_dir, 'vocab.txt')
+    def load_model(self,model_dir):
+        config_name = os.path.join(model_dir, 'bert_config.json')
+        checkpoint_name = os.path.join(model_dir, 'bert_model.ckpt')
+        vocab_name = os.path.join(model_dir, 'vocab.txt')
         self.tokenizer = Tokenizer(vocab_name, do_lower_case=True)  # 建立分词器
         self.model = build_transformer_model(config_name, checkpoint_name)  # 建立模型，加载权重
 
@@ -36,22 +37,28 @@ class ExtractBertFeatures:
 class DownloadBertModel:
     def __init__(self,url='https://storage.googleapis.com/bert_models/2020_02_20/uncased_L-2_H-128_A-2.zip'):
         self.model_dir = url
-        self.name = url.split('/')[-1].split('.')[0]
+        name = url.split('/')[-1].split('.')[0]
+        self.save_parh = os.path.join(CUR_PASTH, 'data', name)
 
-    def download(self,save_parh='.'):
+    def dump(self,save_parh=''):
+        if not save_parh:
+            save_parh = self.save_parh
         if not os.path.exists(save_parh):
             os.makedirs(save_parh)
+
         r = requests.get(self.model_dir)
         with open("_tmp.zip", "wb") as code:
             code.write(r.content)
 
-        file_dir = os.path.join(save_parh,self.name)
         f = zipfile.ZipFile('_tmp.zip', 'r')
         for file in f.namelist():
-            f.extract(file,file_dir)
+            f.extract(file,save_parh)
+        os.remove("_tmp.zip")
+
 
 if __name__ == '__main__':
-    # aa=DownloadBertModel()
-    # aa.download()
+    aa=DownloadBertModel()
+    aa.dump()
+    print(CUR_PASTH)
     bb=ExtractBertFeatures()
     print(bb.predict('你好啊'))
