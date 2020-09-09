2335eb2635cf01a0b1319cd16d540580b823dde8
wqwang <wqwangchn@163.com>
2020-09-09 14:50:01 +0800

    update

--- shell/publish.sh
+++ shell/publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #PYTHONHTTPSVERIFY=0
-
+#twine==3.2.0
 # 1.删除已有包
 #rm -rf ./build
 #rm -rf ./dist
 #rm -rf ./pynovice.egg-info
 
 # 2. add tag
-# git tag v0.2.13 -m 'update feature_pre'
+# git tag v0.2.22 -m 'update feature_pre'
 
 python setup.py sdist bdist_wheel
 python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
